567609f5b7bfc690cb0019a4457893a343510575
nickevansuk <2616208+nickevansuk@users.noreply.github.com>
2018-10-23 00:59:05 +0100

    Fix proto in heroku

--- data/itemGenerator.js
+++ data/itemGenerator.js
@@ -292,7 +292,7 @@ function generateItem(seed, baseUrl) {
 }
 
 function baseUrl(req) {
-  return req.protocol + '://' + req.get('host');
+  return (req.headers["X-Forwarded-Proto"] || req.protocol) + '://' + req.get('host');
 }
 
 module.exports = {
